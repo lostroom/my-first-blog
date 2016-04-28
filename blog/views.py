812578650330879a42b9af005d7ca68cb97bbb18
lostroom <mi.agarkov@gmail.com>
2016-04-28 15:37:22 +0300

    add post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from datetime import datetime
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=datetime.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
