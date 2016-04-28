132e5b13b4aef97a1073fa06767c5892b6f1a034
lostroom <mi.agarkov@gmail.com>
2016-04-28 10:33:29 +0300

    fix datetime

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Post
+from datetime import datetime
 from django.utils import timezone
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=datetime.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
